5cd31a255af7b8cd567868628ae3f641b5fe11b0
perpil <97474956+perpil@users.noreply.github.com>
2024-06-24 14:19:07 -0700

    Initial commit

---
+++ test/dirty_secrets.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DirtySecrets from '../lib/dirty_secrets-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dirty_secrets-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DirtySecrets.DirtySecretsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
